8f07fe026bd2b05fc7386b220683917e7ddbd782
0xlam <208283706+0xlam@users.noreply.github.com>
2026-05-03 10:59:02 +0100

    Add commandExecutor.js

---
+++ commandExecutor.js
@@ -0,0 +1,43 @@
+import { filename } from "./store.js"
+import {  add, list, done, undo, remove, filter, edit, clear, stats, help } from "./taskOperations.js"
+
+function executeCommand(parsed){
+
+    let command = parsed.command
+	
+    switch (command){
+    	case "add":
+    		add(parsed.payload.text);
+    		break;
+    	case "edit":
+    	    edit(parsed.payload.id, parsed.payload.newText);
+    	    break;
+    	case "done":
+    	    done(parsed.payload.ids);
+    	    break;
+    	case "remove":
+    	    remove(parsed.payload.id);
+    	    break;
+    	case "list":
+    	    list();
+    	    break;
+        case "filter":
+            filter(parsed.payload.status);
+            break;
+    	case "undo":
+    	    undo(parsed.payload.id);
+    	    break;
+    	case "clear":
+    	    clear();
+    	    break;
+    	case "stats":
+    	    stats();
+    	    break;
+    	case "help":
+    	    help();
+    	    break;
+
+    }
+}
+
+export { executeCommand };
